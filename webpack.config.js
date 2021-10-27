9e15a350e147df78fc9e59cfe6b4a0f7cc3adc92
mayara-morais <57415644+mayara-morais@users.noreply.github.com>
2021-10-26 22:08:55 -0300

    Problemas
    require('webpack-dev-server') não está sendo reconhecido, npm start não funciona

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,33 @@
 'use strict'
 
 const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
-  entry: path.join(__dirname, 'src', 'index'),
+  devtool: 'source-map', // better debug inspector, important to debug, shows more specif error messages and the file source
+  entry: [
+	'react-hot-loader/patch',
+	'webpack-dev-server/client?http://localhost:3000',
+	'webpack/hot/only-dev-server',
+	path.join(__dirname, 'src', 'index')
+  ],
+   devServer: {
+    port: 3000
+  },
   output:{
 	  path: path.join(__dirname, 'dist'),
 	  filename: 'bundle.js',
 	  publicPath: '/dist/'
   },
+  plugins: [
+		new webpack.HotModuleReplacementPlugin()
+  ],
   module:{
 		loaders:[{
 			test: /\.js$/,
 			exclude: /node_modules/,
 			include: /src/,
-			loader: 'babel'
+			loader: 'babel-loader'
 		}]
   }	
 }	
